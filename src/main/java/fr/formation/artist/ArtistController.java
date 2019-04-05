d7df3c2afc269ce905787801c5c423499883bd92
Nidhal <chouaref.nidhal@gmail.com>
2019-04-05 16:58:10 +0200

    Abstract Controller

--- src/main/java/fr/formation/artist/ArtistController.java
+++ src/main/java/fr/formation/artist/ArtistController.java
@@ -1,5 +1,11 @@
 package fr.formation.artist;
 
 
-public class ArtistController {
+import fr.formation.controller.AbstractController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/artists")
+public class ArtistController extends AbstractController {
 }
--- src/main/java/fr/formation/user/UserController.java
+++ src/main/java/fr/formation/user/UserController.java
@@ -1,5 +1,6 @@
 package fr.formation.user;
 
+import fr.formation.controller.AbstractController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UserController extends AbstractController {
 
 	@Autowired
 	private UserService userService;
@@ -22,7 +23,7 @@ public class UserController {
 	 * @param password the password
 	 * @param roles    the roles
 	 */
-	@PostMapping("/")
+	@PutMapping("/")
 	public void signup(@RequestParam String username, @RequestParam String password,
 					   @RequestParam String mail, @RequestParam String city,
 					   @RequestParam String... roles) {
