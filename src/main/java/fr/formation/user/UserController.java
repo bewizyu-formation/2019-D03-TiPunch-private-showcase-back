ef9ed35bbc88670e63f49170c8408a0d2aca6d32
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-04-10 11:36:52 +0200

    return status apres put user et artist

--- src/main/java/fr/formation/user/UserController.java
+++ src/main/java/fr/formation/user/UserController.java
@@ -4,8 +4,13 @@ import fr.formation.artist.ArtistService;
 import fr.formation.controller.AbstractController;
 import fr.formation.modelDto.ArtistDto;
 import fr.formation.modelDto.UserDto;
-import fr.formation.models.User;
+import fr.formation.security.SecurityConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,27 +26,34 @@ public class UserController extends AbstractController {
 	@Autowired
 	private ArtistService artistService;
 
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	/**
 	 * Signup.
 	 *
-	 * @param username the username
-	 * @param password the password
-	 * @param roles    the roles
 	 */
 	@PutMapping(value = "/")
-	public void signup(@RequestBody UserDto data) {
+	public ResponseEntity signup(@RequestBody UserDto data) {
 
+		boolean addUser = userService.addNewUser(data.getUsername(), data.getPassword(), data.getMail(), data.getCity());
 
-		userService.addNewUser(data.getUsername(), data.getPassword(), data.getMail(), data.getCity() );
+		if(addUser) return new ResponseEntity(HttpStatus.CREATED);
+
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 
 	}
 
 	@PutMapping("/artist/")
-	public void signup (@RequestBody ArtistDto artist){
+	public ResponseEntity signup (@RequestBody ArtistDto artist){
+		boolean addArtist = artistService.addNewArtist(artist.getUsername(), artist.getPasswordArtist(),
+				artist.getMailArtist(), artist.getCityArtist(), artist.getNameArtist(),artist.getDescriptionArtist());
 
-	    artistService.addNewArtist(artist.getUsername(), artist.getPasswordArtist(), artist.getMailArtist(), artist.getCityArtist(), artist.getNameArtist(),artist.getDescriptionArtist());
-        userService.addNewUser(artist.getUsername(), artist.getPasswordArtist(), artist.getMailArtist(), artist.getCityArtist() );
+        boolean addUser = userService.addNewUser(artist.getUsername(), artist.getPasswordArtist(), artist.getMailArtist(), artist.getCityArtist() );
 
+        if (addArtist && addUser) return new ResponseEntity(HttpStatus.CREATED);
+
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
+
 
 }
